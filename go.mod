1726daf4dc59f488c43f3647a04bb646089299da
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-16 11:30:26 +0700

    chore: initialize dotfile

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/ryansuhartanto/koda-b8-ewallet-cli
+
+go 1.26.5
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("Error loading .env file", err)
+	}
+
+	fmt.Println(os.Getenv("PGDATABASE"))
 }
