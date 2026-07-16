1e81f214b8f77f3642c0972cc15f04d88576a11e
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-16 11:59:26 +0700

    feat: initialize schema

---
+++ db/migrations/000001_init.down.sql
@@ -0,0 +1,17 @@
+DROP TABLE entries;
+
+DROP TABLE transactions;
+
+DROP TYPE transaction_status;
+
+DROP TYPE transaction_type;
+
+DROP TABLE users_wallets;
+
+DROP TABLE wallets;
+
+DROP TABLE users_spi;
+
+DROP TABLE users;
+
+DROP FUNCTION "update_updated_at"();
---
+++ db/migrations/000001_init.up.sql
@@ -0,0 +1,109 @@
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    display_name VARCHAR NOT NULL
+);
+
+CREATE TABLE users_spi (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    id_user BIGINT NOT NULL UNIQUE REFERENCES users (id),
+    verified_at TIMESTAMP,
+
+    ssn CHAR(16) NOT NULL UNIQUE,
+    legal_name VARCHAR NOT NULL,
+    dob DATE NOT NULL,
+
+    tax_id VARCHAR
+);
+
+CREATE TABLE wallets (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    balance_idr BIGINT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE users_wallets (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    id_user BIGINT NOT NULL REFERENCES users (id),
+    id_wallet BIGINT NOT NULL REFERENCES wallets (id)
+);
+
+CREATE TYPE transaction_type AS ENUM ('topup', 'withdraw', 'transfer', 'payment');
+
+CREATE TYPE transaction_status AS ENUM ('pending', 'success', 'failed');
+
+CREATE TABLE transactions (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    type transaction_type NOT NULL,
+
+    ref_internal VARCHAR NOT NULL,
+    ref_external VARCHAR,
+    provider VARCHAR,
+    note VARCHAR,
+
+    status transaction_status NOT NULL DEFAULT 'pending'
+);
+
+CREATE TABLE entries (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    id_wallet BIGINT NOT NULL REFERENCES wallets (id),
+    id_transaction BIGINT NOT NULL REFERENCES transactions (id),
+
+    amount BIGINT NOT NULL,
+    balance_idr_after BIGINT NOT NULL
+);
+
+--
+
+CREATE FUNCTION "update_updated_at"()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
+      NEW."updated_at" = CURRENT_TIMESTAMP;
+      RETURN NEW;
+   ELSE
+      RETURN OLD;
+   END IF;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER "users_updated_at"
+BEFORE UPDATE ON "users"
+FOR EACH ROW EXECUTE PROCEDURE "update_updated_at"();
+
+CREATE TRIGGER "users_spi_updated_at"
+BEFORE UPDATE ON "users_spi"
+FOR EACH ROW EXECUTE PROCEDURE "update_updated_at"();
+
+CREATE TRIGGER "wallets_updated_at"
+BEFORE UPDATE ON "wallets"
+FOR EACH ROW EXECUTE PROCEDURE "update_updated_at"();
+
+CREATE TRIGGER "transactions_updated_at"
+BEFORE UPDATE ON "transactions"
+FOR EACH ROW EXECUTE PROCEDURE "update_updated_at"();
