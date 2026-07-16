58213df6fd3cf48368e0b18300f37b714b891cf0
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-16 14:18:54 +0700

    fix: close connection

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	if err != nil {
 		log.Panicln("Error connecting to database", err)
 	}
+	defer conn.Close(ctx)
 
 	query := `SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE' ORDER BY table_name;`
 	rows, err := conn.Query(ctx, query)
