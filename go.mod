258317844903639cf5a458fd196895ef7f0a89f3
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-16 13:24:02 +0700

    feat: apply migration on app

--- go.mod
+++ go.mod
@@ -3,9 +3,16 @@ module github.com/ryansuhartanto/koda-b8-ewallet-cli
 go 1.26.5
 
 require (
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.10.0
 	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,61 @@ package main
 
 import (
 	"context"
+	"embed"
+	"fmt"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
+//go:embed db/migrations/*
+var migrationsFS embed.FS
+
+func init() {
+	d, err := iofs.New(migrationsFS, "db/migrations")
+	if err != nil {
+		log.Panicln("Error creating FS driver", err)
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", d, "pgx5://:")
+	if err != nil {
+		log.Panicln("Error creating migration instance", err)
+	}
+
+	err = m.Up()
+	if err != nil {
+		log.Panicln("Error migrating database", err)
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Panic("Error loading .env file", err)
+		log.Panicln("Error loading .env file", err)
 	}
 
-	_, err = pgx.Connect(context.Background(), "")
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, "")
 	if err != nil {
-		log.Panic("Error connecting to database", err)
+		log.Panicln("Error connecting to database", err)
 	}
+
+	query := `SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE' ORDER BY table_name;`
+	rows, err := conn.Query(ctx, query)
+	if err != nil {
+		log.Fatalln("Query failed", err)
+	}
+	defer rows.Close()
+
+	tables, err := pgx.CollectRows(rows, pgx.RowTo[string])
+	if err != nil {
+		log.Fatalln("Failed to collect rows", err)
+	}
+
+	fmt.Println("Tables:", tables)
 }
